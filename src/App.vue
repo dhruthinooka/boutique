66a164a60f3b2ec2d1bc3bbf197eb024ed174210
Dhruthi <dhruthi74@gmail.com>
2025-10-21 09:26:17 +0200

    Overall layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
-
+  import TheHeader from './components/Header.vue';
+  import TheFooter from './components/Footer.vue';
+  import Shop from './components/Shop.vue';
+  import Cart from './components/Cart.vue';
 </script>
 
 <template>
-<h1>Bonjour le monde!</h1>
+  <div class="app-container">
+    <TheHeader class="header b1" />
+    <TheFooter class="footer b2" />
+    <Shop class="shop b3" />
+    <Cart class="cart b4" />
+  </div>
+
 </template>
 
 <style lang="scss">
 @import "assets/base.scss";
 @import "assets/debug.scss";
 
+.app-container {
+  min-height: 100vh;
+  display: grid;
+  grid-template-areas: "header header" "shop cart" "footer footer";
+  grid-template-columns: 75% 25%;
+  grid-template-rows: 48px auto 48px;
+}
+
+.header {
+  grid-area: header;
+}
+
+.shop {
+  grid-area: shop;
+}
+
+.cart {
+  grid-area: cart;
+}
+
+.footer {
+  grid-area: footer;
+}
 </style>
