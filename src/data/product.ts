31fa6a75ba9a530d338e601258bc1063d3493123
Dhruthi <dhruthi74@gmail.com>
2025-10-22 08:59:09 +0200

    Added few more products

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,20 +1,66 @@
 export default [
   {
+    id: 1,
     image: 'src/assets/images/macbookpro.jpg',
     title: 'Macbook Pro',
-    description: 'Lorem ipsum dolor sit amet, consectetur ',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
     price: 1500,
   },
   {
+    id: 2,
     image: 'src/assets/images/lenovopro.jpg',
-    title: 'Lenovo Pro',
-    description: 'Lorem ipsum dolor sit amet, consectetur ',
+    title: 'Levono Pro',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
     price: 2300,
   },
   {
-    image: 'src/assets/images/asus.jpg',
-    title: 'Asus',
-    description: 'Lorem ipsum dolor sit amet, consectetur ',
+    id: 3,
+    image: 'src/assets/images/rider.jpg',
+    title: 'Rider',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
+    price: 1200,
+  },
+  {
+    id: 4,
+    image: 'src/assets/images/ldlc.jpg',
+    title: 'LDLC benolo',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
     price: 4500,
-  }
-]
+  },
+  {
+    id: 5,
+    image: 'src/assets/images/asus.jpg',
+    title: 'Asus gamer',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
+    price: 3755,
+  },
+  {
+    id: 6,
+    image: 'src/assets/images/rog.png',
+    title: 'Rog desktop',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
+    price: 2452,
+  },
+  {
+    id: 7,
+    image: 'src/assets/images/msi.jpg',
+    title: 'MSI play',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
+    price: 1478,
+  },
+  {
+    id: 8,
+    image: 'src/assets/images/pad.jpg',
+    title: 'Think pad',
+    description:
+      'Lorem ipsum dolor sit amet consectetur',
+    price: 899,
+  },
+];
